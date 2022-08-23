dcb78a94f7d96d7a8d3e66f107fd6564fb6e6fed
void-ness <78547746+void-ness@users.noreply.github.com>
2022-08-23 15:36:12 +0530

    updated: home-page

--- src/pages/home/footer/index.jsx
+++ src/pages/home/footer/index.jsx
@@ -7,15 +7,15 @@ const Footer = () => {
             <div className="footer-container position-fixed bottom-0 w-100">
                 <div className="pg-breaker"></div>
                 <div className="footer-bar d-flex align-items-center justify-content-center mt-1 mb-2">
-                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center" to='/notes'>
+                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center text-decoration-none" to='/notes'>
                         <i className="fa-regular fa-file p-1"></i>
                         Notes
                     </Link>
-                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center" to='/books'>
+                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center text-decoration-none" to='/books'>
                         <i className="fa-solid fa-book p-1"></i>
                         Books
                     </Link>
-                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center" to='/account'>
+                    <Link className="footer-btn d-flex flex-column justify-content-center align-items-center text-decoration-none" to='/account'>
                         <i className="fa-regular fa-user p-1"></i>
                         Account
                     </Link>
--- src/pages/home/navbar/index.jsx
+++ src/pages/home/navbar/index.jsx
@@ -4,10 +4,11 @@ const Navbar = () => {
     return (
         <>
             <h2 className="home_heading fs-butterfly bg-yellow text-center p-2">Notesgenix</h2>
-            <div className="home_nav">
-                <button>Personal</button>
-                <button>Shared</button>
+            <div className="home_nav my-2 d-flex justify-content-center align-items-center">
+                <button className='bg-transparent border-0 mx-3 w-50 fs-5 text-gray'>Personal</button>
+                <button className='bg-transparent border-0 mx-3 w-50 fs-5 text-gray'>Shared</button>
             </div>
+            <div className="pg-breaker"></div>
         </>
     )
 };
