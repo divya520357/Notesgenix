b87a9838379b38d5e029e82d51ec5e32f8069492
void-ness <78547746+void-ness@users.noreply.github.com>
2022-08-23 09:47:03 +0530

    added: login page

---
+++ src/pages/login/Landing/index.jsx
@@ -0,0 +1,22 @@
+import './style.css';
+import logo from './assets/logo.png';
+
+const Landing = () => {
+    function screensplash() {
+        setTimeout(() => {
+            document.getElementById("landing-page").style.opacity = "0";
+        }, 4000);
+    }
+
+    screensplash();
+    return (
+        <>
+            <div className="landingpage_container" id="landing-page">
+                <img src={logo} alt="" className="landingpage_logo my-3 mt-0" data-aos="zoom-in-up" data-aos-duration="1500" />
+                <span className="landingpage_text" data-aos="fade-up" data-aos-delay="1500" data-aos-duration="1000">Notesgenix</span>
+            </div>
+        </>
+    )
+};
+
+export default Landing;
