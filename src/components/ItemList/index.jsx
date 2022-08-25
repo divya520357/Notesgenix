f6a40c59f3f4af8e7a813bcc9ad1364db32fc6a9
void-ness <78547746+void-ness@users.noreply.github.com>
2022-08-25 22:04:17 +0530

    added: white overlay

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,19 +1,23 @@
 import './style.css';
 
 import ItemCard from './Item';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const ItemList = ({ data }) => {
     const overlay = useRef(null);
+    const overlayBg = useRef(null);
+    const [noteName, SetNoteName] = useState(null);
     let timer;
 
-    const TouchStart = (e) => {
-        e.preventDefault();
+    const TouchStart = (notesName) => {
         if (!timer) {
             // A timeout function for adding long touch functionality
             timer = setTimeout(() => {
+                SetNoteName(notesName);
                 overlay.current.style.bottom = '0';
-                overlay.current.style.transition = 'all ease-out 0.6s'
+                overlay.current.style.transition = 'all ease-out 0.6s';
+                overlayBg.current.style.transition = 'all ease-out 2s';
+                overlayBg.current.style.zIndex = '1';
             }, 250)
         }
     };
@@ -28,24 +32,26 @@ const ItemList = ({ data }) => {
     const closeOverlay = () => {
         overlay.current.style.bottom = '-100%';
         overlay.current.style.transition = 'all ease-in 0.6s'
+        overlayBg.current.style.transition = 'all ease-in 0.1s'
+        overlayBg.current.style.zIndex = '-1';
     }
 
     return (
         <>
-            <div className="container home_notes_container position-absolute d-flex flex-wrap justify-content-evenly ">
+            <div className="container home_notes_container position-relative d-flex flex-wrap justify-content-evenly">
                 {data.map((item, i) => (
-                    <ItemCard key={i} data={item} 
+                    <ItemCard key={i} data={item}
                         onTouchStart={TouchStart}
                         onTouchEnd={TouchEnd}
                     />
                 ))}
             </div>
-            
 
+            <div ref={overlayBg} onClick={closeOverlay} className="card-overlay-bg position-fixed w-100 h-100 bg-red"></div>
             <div ref={overlay} className="card-overlay bg-yellow position-fixed w-100 h-50 d-flex flex-column">
                 <div className="w-75 d-flex justify-content-start mt-3 mb-4 ms-3 align-items-center">
                     <i onClick={closeOverlay} className="fa-solid fa-angle-left fa-lg pe-3 ps-1"></i>
-                    <span className="text-start text-gray fs-3">Notes</span>
+                    <span className="text-start text-gray fs-3">{noteName}</span>
                 </div>
 
                 <div className="w-75 d-flex justify-content-start my-2 ms-3 align-items-center">
@@ -69,7 +75,7 @@ const ItemList = ({ data }) => {
                 </div>
             </div>
 
-            
+
         </>
     )
 };
